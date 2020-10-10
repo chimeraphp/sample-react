08779b7659a5e3fe7ebdaf05c8e64cf344aed278
Luís Cobucci <lcobucci@gmail.com>
2020-10-10 19:48:34 +0200

    Check if endpoints work properly

--- public/index.php
+++ public/index.php
@@ -7,12 +7,14 @@ use Chimera\Routing\Application;
 use Psr\Container\ContainerInterface;
 use Psr\Log\LoggerInterface;
 use React\EventLoop\LoopInterface;
-use React\Http\Middleware\StreamingRequestMiddleware;
 use React\Http\Server;
 use React\Socket\Server as SocketServer;
 use Throwable;
-use const PHP_EOL;
+
 use function assert;
+
+use const PHP_EOL;
+use const SIGINT;
 
 $container = require __DIR__ . '/../config/container.php';
 assert($container instanceof ContainerInterface);
@@ -23,20 +25,27 @@ assert($app instanceof Application);
 $loop   = $container->get(LoopInterface::class);
 $logger = $container->get(LoggerInterface::class);
 
-$server = new Server($loop, new StreamingRequestMiddleware(), [$app, 'handle']);
+$server = new Server($loop, [$app, 'handle']);
 
 $server->on(
     'error',
     static function (Throwable $e) use ($logger): void {
-        $logger->error('{message}', ['message' => $e->getMessage()]);
-        $logger->debug((string) $e);
+        $logger->error('{message}' . PHP_EOL, ['message' => $e->getMessage()]);
+        $logger->debug($e . PHP_EOL);
     }
 );
 
 $socket = new SocketServer('tcp://0.0.0.0:80', $loop);
 $server->listen($socket);
 
-echo 'Listening on http://0.0.0.0:80', PHP_EOL;
-echo 'Press Ctrl-C to quit.', PHP_EOL;
+$logger->info('Listening on http://0.0.0.0:80' . PHP_EOL);
+$logger->info('Press Ctrl-C to quit.' . PHP_EOL);
+
+$loop->addSignal(SIGINT, $handler = static function () use ($logger, $socket, $loop, &$handler): void {
+    $logger->info('Shutting down server' . PHP_EOL);
+
+    $socket->close();
+    $loop->removeSignal(SIGINT, $handler);
+});
 
 $loop->run();
